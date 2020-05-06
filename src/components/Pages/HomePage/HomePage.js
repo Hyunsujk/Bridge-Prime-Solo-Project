fb00acf913c757f5955438aec173f2698b1ebf09
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-05-06 14:57:01 -0500

    update layout of homepage

--- src/components/Pages/HomePage/HomePage.js
+++ src/components/Pages/HomePage/HomePage.js
@@ -3,29 +3,53 @@ import { connect } from "react-redux";
 import mapStoreToProps from "../../../redux/mapStoreToProps";
 import LoginForm from "../../LoginForm/LoginForm";
 import homepageImage from "../../img/homepage.jpg";
-import { Container, Typography } from "@material-ui/core";
+import {
+  Container,
+  Typography,
+  Grid,
+  withStyles,
+  createStyles,
+} from "@material-ui/core";
 
+const customStyles = (theme) =>
+  createStyles({
+    root: {
+      textAlign: "left",
+    },
+    body: {
+      margin: `80px 100px 0`,
+      maxWidth: "500px",
+    },
+    image: {
+      margin: `13px 0 0 0`,
+    },
+  });
 class HomePage extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <Container maxWidth={false}>
-        <div>
-          <div>
-            <Typography component="h2" variant="h4">
-              Welcome to Bridge!
-            </Typography>
-            <Typography component="h3" variant="h5">
-              Where homeowners and repairmen connect
-            </Typography>
-          </div>
-          <LoginForm />
-        </div>
-        <div>
-          <img src={homepageImage} width="300" alt="repair" />
-        </div>
+        <Grid container spacing={2} className={classes.root}>
+          <Grid item xs={8} className={classes.body}>
+            <div>
+              <Typography component="h1" variant="h4">
+                Welcome to Bridge!
+              </Typography>
+              <Typography component="h2" variant="h5">
+                Where homeowners and repairmen connect
+              </Typography>
+            </div>
+            <LoginForm />
+          </Grid>
+          <Grid item xs={4} className={classes.image}>
+            <div>
+              <img src={homepageImage} width="300" alt="repair" />
+            </div>
+          </Grid>
+        </Grid>
       </Container>
     );
   }
 }
 
-export default connect(mapStoreToProps)(HomePage);
+export default withStyles(customStyles)(connect(mapStoreToProps)(HomePage));
