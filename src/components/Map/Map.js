005e75aaa6f2a832bd70a0b827fcf3c3eca61867
Hyunsujk <hyunsujk@gmail.com>
2020-05-07 19:32:58 -0500

    created saga for open cage and created component files

---
+++ src/components/Map/Map.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
+import MapBoxComponent from "../MapBoxComponent/MapBoxComponent";
+
+class Map extends Component {
+  onChange = (key) => (event) => {
+    this.setState(
+      {
+        ...this.state,
+        [key]: event.target.value,
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+  search = (event) => {
+    this.props.dispatch({
+      type: "FETCH_OPEN_CAGE",
+      payload: { search_string: this.state.search_string },
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input type="text" onChange={this.onChange("search_string")}></input>
+          <button onClick={this.search}>SEARCH</button>
+        </div>
+
+        <div>
+          <MapBoxComponent />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(Map);
--- src/components/Pages/LandingPage/LandingPage.js
+++ src/components/Pages/LandingPage/LandingPage.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
+import MapView from "../../View/MapView/MapView";
 
 class LandingPage extends Component {
   render() {
     return (
       <div>
         <h2>Landing Page</h2>
+        <MapView />
       </div>
     );
   }
