aabb100e17ef8afc6faa496a315f2f823cd8d0da
Hyunsujk <hyunsujk@gmail.com>
2020-05-07 23:44:39 -0500

    installed react-map-gl, rendered map on landing page with kansas city lnglat. test good

---
+++ src/components/MapBoxComponent/MapBoxComponent.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import ReactMapGL from "react-map-gl";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+
+class MapBoxComponent extends Component {
+  state = {
+    viewport: {
+      latitude: 39.100105,
+      longitude: -94.5781416,
+      zoom: 11,
+      bearing: 0,
+      pitch: 0,
+    },
+  };
+
+  viewportChange = (change) => {
+    this.setState({
+      viewport: change,
+    });
+  };
+
+  clickMap = (event) => {
+    console.log("lng: ", event.lngLat[0]);
+    console.log("lat: ", event.lngLat[1]);
+  };
+
+  forceUpdate() {
+    this.props.dispatch({ type: "MAP_FORCE_UPDATE_ENFORCED" });
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        latitude: this.props.store.cord.lat,
+        longitude: this.props.store.cord.lng,
+      },
+    });
+  }
+
+  render() {
+    if (this.props.store.cord.updateNeeded) {
+      this.forceUpdate();
+    }
+
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        width="70vw"
+        height="70vh"
+        mapStyle="mapbox://styles/mapbox/outdoors-v11"
+        onViewportChange={this.viewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        onClick={this.clickMap}
+      />
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(MapBoxComponent);
--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,7 +3,7 @@ import errors from "./errors.reducer";
 import loginMode from "./loginMode.reducer";
 import user from "./user.reducer";
 import criteria from "./criteria.reducer";
-
+import cord from "./cord.reducer";
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -15,6 +15,7 @@ const rootReducer = combineReducers({
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   user, // will have an id and username if someone is logged in
   criteria, // will have criteria details for repairman
+  cord,
 });
 
 export default rootReducer;
