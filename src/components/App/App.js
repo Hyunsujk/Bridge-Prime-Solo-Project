eeaf071a2b73128f0276d9de15b3e711c7edc5b0
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-05-07 21:09:17 -0500

    trying to guide user to landing page once they are logged in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,7 +16,7 @@ import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 import AboutPage from "../AboutPage/AboutPage";
 import UserPage from "../UserPage/UserPage";
 import InfoPage from "../InfoPage/InfoPage";
-import LandingPage from "../LandingPage/LandingPage";
+import LandingPage from "../Pages/LandingPage/LandingPage";
 import LoginPage from "../LoginPage/LoginPage";
 import RegisterPage from "../RegisterPage/RegisterPage";
 import HomeownerRegistrationPage from "../Pages/HomeownerRegistrationPage/HomeownerRegistrationPage";
@@ -46,12 +46,11 @@ class App extends Component {
             This is a route anyone can see, no login necessary */}
             <Route exact path="/about" component={AboutPage} />
             <Route exact path="/home" component={HomePage} />
-            <Route exact path="/landing" component={LandingPage} />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-            <ProtectedRoute exact path="/admin" component={UserPage} />
+            <ProtectedRoute exact path="/landing" component={LandingPage} />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
             <ProtectedRoute exact path="/info" component={InfoPage} />
@@ -60,25 +59,25 @@ class App extends Component {
             <ProtectedRoute
               exact
               path="/login"
-              authRedirect="/admin"
+              authRedirect="/landing"
               component={LoginPage}
             />
             <ProtectedRoute
               exact
               path="/registration"
-              authRedirect="/admin"
+              authRedirect="/landing"
               component={RegistrationMainPage}
             />
             <ProtectedRoute
               exact
               path="/hregistration"
-              authRedirect="/admin"
+              authRedirect="/landing"
               component={HomeownerRegistrationPage}
             />
             <ProtectedRoute
               exact
               path="/rregistration"
-              authRedirect="/admin"
+              authRedirect="/landing"
               component={RepairmanRegistrationPage}
             />
 
