12f874c841ace762745ca6d15d211766362dc55b
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-05-07 19:32:58 -0500

    created saga for open cage and created component files

---
+++ src/components/View/MapView/MapView.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
+import Map from "../../Map/Map";
+
+class MapView extends Component {
+  render() {
+    return (
+      <div>
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(MapView);
